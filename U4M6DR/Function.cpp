24737ff85a819dbed30aa60cbaa1273125ad5b4d
zruslan2@yandex.ru <zruslan2@yandex.ru>
2018-02-11 19:16:18 +0600

    Add project files.

---
+++ U4M6DR/Function.cpp
@@ -0,0 +1,146 @@
+#include <vector>
+#include <time.h>
+#include <conio.h>
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+int i, j;
+
+void head(char mas[])
+{
+	int len, third, s1_len, s2_len, s3_len, maks;
+	len = strlen(mas);
+	third = len / 3;
+	s1_len = third;
+	for (int i = s1_len; i < len; i++)
+	{
+		if (mas[s1_len] != ' ') s1_len++;
+		else break;
+	}
+	s2_len = third;
+	for (int i = s1_len + s2_len; i < len; i++)
+	{
+		if (mas[s1_len + s2_len] != ' ') s2_len++;
+		else break;
+	}
+	s3_len = len - (s1_len + s2_len);
+	vector<char>str1(s1_len);
+	vector<char>str2(s2_len);
+	vector<char>str3(s3_len);
+	for (int i = 0; i < s1_len; i++)
+	{
+		str1[i] = mas[i];
+	}
+	for (int i = s1_len + 1; i < s1_len + s2_len; i++)
+	{
+		str2[i - (s1_len + 1)] = mas[i];
+	}
+	for (int i = s1_len + s2_len + 1; i < len; i++)
+	{
+		str3[i - (s1_len + s2_len + 1)] = mas[i];
+	}
+	maks = s1_len;
+	if (s2_len > maks)maks = s2_len;
+	if (s3_len > maks)maks = s3_len;
+	system("cls");
+	SetConsoleTextAttribute(hConsole, 11);
+	setlocale(LC_ALL, ".866");
+	cout << " Ú";
+	for (int i = 0; i <= maks + 1; i++)cout << "Ä";
+	cout << "¿\n";
+	cout << " ³ "; setlocale(LC_ALL, ".1251");
+	for (int i = 0; i<s1_len; i++) cout << str1[i];
+	if (s1_len < maks)
+	{
+		for (int i = 0; i < maks - s1_len; i++)cout << " ";
+	}
+	setlocale(LC_ALL, ".866"); cout << " ³\n";
+	cout << " ³ "; setlocale(LC_ALL, ".1251");
+	for (int i = 0; i<s2_len; i++) cout << str2[i];
+	if (s2_len < maks)
+	{
+		for (int i = 0; i < maks - s2_len; i++)cout << " ";
+	}
+	setlocale(LC_ALL, ".866"); cout << " ³\n";
+	cout << " ³ "; setlocale(LC_ALL, ".1251");
+	for (int i = 0; i<s3_len; i++) cout << str3[i];
+	if (s3_len < maks)
+	{
+		for (int i = 0; i < maks - s3_len; i++)cout << " ";
+	}
+	setlocale(LC_ALL, ".866"); cout << " ³\n";
+	cout << " À";
+	for (int i = 0; i <= maks + 1; i++)cout << "Ä";
+	cout << "Ù\n";
+	SetConsoleTextAttribute(hConsole, 15);
+	setlocale(LC_ALL, ".1251");
+}
+
+void yellow()
+{
+	SetConsoleTextAttribute(hConsole, 14);
+}
+
+void white()
+{
+	SetConsoleTextAttribute(hConsole, 15);
+}
+
+void red()
+{
+	SetConsoleTextAttribute(hConsole, 12);
+}
+
+void green()
+{
+	SetConsoleTextAttribute(hConsole, 11);
+}
+
+double f(double x, double y)
+{
+	return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+double f(double x1, double y1, double x2, double y2)
+{
+	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int f(int a)
+{
+	cout << "Ôóíêöèÿ 1\n";
+	return 0;
+}
+
+int f(int a, int b)
+{
+	cout << "Ôóíêöèÿ 2\n";
+	return 0;
+}
+
+int f(int a, int b, int c)
+{
+	cout << "Ôóíêöèÿ 3\n";
+	return 0;
+}
+
+void swap(int* a, int* b)
+{
+	int prom;
+	prom = *a;
+	*a = *b;
+	*b = prom;
+}
+
+void swap(int& a, int& b)
+{
+	int prom;
+	prom = a;
+	a = b;
+	b = prom;
+}
---
+++ U4M6DR/MyFunc.h
@@ -0,0 +1,12 @@
+void head(char mas[]);
+void yellow();
+void white();
+void red();
+void green();
+double f(double x, double y);
+double f(double x1, double y1, double x2, double y2);
+int f(int a);
+int f(int a, int b);
+int f(int a, int b, int c);
+void swap(int* a, int* b);
+void swap(int& a, int& b);
